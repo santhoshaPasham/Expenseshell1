ad574f1399b03bc44a64d0d03207b857e8a8b804
pasham7 <santhoshapasham728@gmail.com>
2024-05-07 22:19:40 +0530

    Set common

--- backend.sh
+++ backend.sh
@@ -10,13 +10,13 @@ read root_password
 
 
 dnf module disable nodejs -y &>>$LOGFILE
-VALIDATE $? "Disabling nodejs"
+#VALIDATE $? "Disabling nodejs"
 
 dnf module enable nodejs:20 -y &>>$LOGFILE
-VALIDATE $? "Enabling nodejs"
+#VALIDATE $? "Enabling nodejs"
 
 dnf install nodejs -y &>>$LOGFILE
-VALIDATE $? "Installing nodejs"
+#VALIDATE $? "Installing nodejs"
 
 
 id expense &>>$LOGFILE
@@ -24,48 +24,48 @@ id expense &>>$LOGFILE
 if [ $? -ne 0 ]
 then 
     useradd expense &>>$LOGFILE
-    VALIDATE $? "Creating user"
+    #VALIDATE $? "Creating user"
 else
     echo -e "Already user exists $Y Skipping $N"
 fi 
 
 mkdir -p /app &>>$LOGFILE
-VALIDATE $? "Creating app directory"
+#VALIDATE $? "Creating app directory"
 
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
-VALIDATE $? "Downloading backend code"
+#VALIDATE $? "Downloading backend code"
 
 cd /app
 
 
 rm -rf /app/*
 unzip /tmp/backend.zip &>>$LOGFILE
-VALIDATE $? "Extracted backend code"
+#VALIDATE $? "Extracted backend code"
 
 npm install &>>$LOGFILE
-VALIDATE $? "Installing nodejs dependencies"
+#VALIDATE $? "Installing nodejs dependencies"
 
 cp /home/ec2-user/Expenseshell1/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
-VALIDATE $? "Coped backend service"
+#VALIDATE $? "Coped backend service"
 
 systemctl daemon-reload &>>$LOGFILE
-VALIDATE $? "Daemon reload"
+#VALIDATE $? "Daemon reload"
 
 systemctl start backend &>>$LOGFILE
-VALIDATE  $? "Starting backend"
+#VALIDATE  $? "Starting backend"
 
 systemctl enable backend &>>$LOGFILE
-VALIDATE $? "Enabling backend"
+#VALIDATE $? "Enabling backend"
 
 dnf install mysql -y &>>$LOGFILE
-VALIDATE $? "Install mysql client"
+#VALIDATE $? "Install mysql client"
 
 mysql -h  db.daws9.online -uroot -p${root_password} < /app/schema/backend.sql &>>$LOGFILE
-VALIDATE $? "Schema loading"
+#VALIDATE $? "Schema loading"
 
 systemctl restart backend &>>$LOGFILE
-VALIDATE $? "Restart backend"
+#VALIDATE $? "Restart backend"
 
 
 

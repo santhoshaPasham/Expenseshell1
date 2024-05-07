c02b1642bb8a4292560f04cba5ec590216549298
pasham7 <santhoshapasham728@gmail.com>
2024-05-07 17:54:20 +0530

    mysqlcommon1

--- frontend.sh
+++ frontend.sh
@@ -1,32 +1,9 @@
 #!/bin/bash 
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%S)
-SCRIPTNAME=$( echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
+source ./common.sh
 
+check_root
 
-if [ $USERID -ne 0 ]
-then 
-    echo "Please run this command with root access"
-    exit 1
-else
-    echo "you are super user"
-fi 
-
-
-VALIDATE(){
-if [ $1 -ne 0 ]
-then 
-    echo -e "$2 is $R failure $N"
-    exit 1
-else 
-    echo -e "$2 is $G success $N"
-fi
-}
 
 dnf install nginx -y &>>$LOGFILE
 VALIDATE $? "Installing nginx"
